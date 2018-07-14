6f7c669cbfa289481fc2d28e10ffe37f8f1316f8
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-07-14 23:17:04 +0200

    Add error reporting via sentry

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue';
+import VueSentry from 'vue2-sentry';
+
 import App from './App.vue';
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueSentry, {
+    key: process.env.VUE_APP_SENTRY_PUBLIC_KEY,
+    project: process.env.VUE_APP_SENTRY_PROJECT_ID,
+  });
+}
 
 Vue.config.productionTip = false;
 
