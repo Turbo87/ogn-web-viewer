a09b54197d86ab0570657f471ede2077bec052e1
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-02-01 11:58:15 +0100

    Add loading spinner for initial page load

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,12 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   setupController(controller) {
     this._super(...arguments);
+
+    // remove loading spinner from the page (see `index.html`)
+    let spinnner = document.querySelector('#initial-load-spinner');
+    spinnner.classList.add('fade');
+    setTimeout(() => spinnner.remove(), 1500);
+
     controller.loadDataTask.perform();
   },
 });
