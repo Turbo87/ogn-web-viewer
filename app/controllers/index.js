8e31cf46c7dad3539d3bea0753972f2ccd7e7416
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-02-02 19:56:18 +0100

    components/map: Move `fit(extent)` code into `index` controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,13 @@ import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 import { task } from 'ember-concurrency';
 
+import { scaleFromCenter } from 'ol/extent';
+import { transformExtent } from 'ol/proj';
+
 import fetchText from 'ogn-web-viewer/utils/fetch-text';
+
+const EPSG_4326 = 'EPSG:4326';
+const EPSG_3857 = 'EPSG:3857';
 
 export default Controller.extend({
   filter: service(),
@@ -30,7 +36,15 @@ export default Controller.extend({
   async loadTask(url) {
     if (url) {
       let { readTaskFromString } = await import('aeroscore/dist/src/read-task');
-      this.set('task', readTaskFromString(await fetchText(url)));
+      let task = readTaskFromString(await fetchText(url));
+      this.set('task', task);
+
+      // zoom map in on the task area
+      let bbox = task.bbox.slice();
+      scaleFromCenter(bbox, 0.3);
+      let extent = transformExtent(bbox, EPSG_4326, EPSG_3857);
+
+      this.mapService.map.getView().fit(extent);
     }
   },
 });
