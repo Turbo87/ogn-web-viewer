ad8e98f0b83ecae3d0c23ee9ae78f0f4facafdc4
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-05-01 14:14:24 +0200

    routes/index: Move result handling into `setupController()` hook

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { run } from '@ember/runloop';
 
 import { scaleFromCenter } from 'ol/extent';
 import { transformExtent } from 'ol/proj';
@@ -20,37 +21,43 @@ export default class extends Route {
     let hash = location.hash || '';
     let params = new URLSearchParams(hash.substr(1));
 
-    await Promise.all([this.loadDeviceFilter(params.get('lst')), this.loadTask(params.get('tsk'))]);
+    return Promise.all([this.loadDeviceFilter(params.get('lst')), this.loadTask(params.get('tsk'))]);
+  }
 
+  setupController(controller, [filter, task]) {
+    run(() => this.scoring.set('task', task));
     this.mapService.map.updateSize();
-  }
 
-  async loadDeviceFilter(url) {
-    if (url) {
-      await this.filter.load(url);
-
-      if (this.filter.hasFilter) {
-        for (let row of this.filter.filter) {
-          this.ws.subscribeToId(row.ID);
-        }
-
-        this.history.loadForIds(...this.filter.filter.map(row => row.ID));
-      }
-    }
-  }
-
-  async loadTask(url) {
-    if (url) {
-      let { readTaskFromString } = await import('aeroscore/dist/src/read-task');
-      let task = readTaskFromString(await fetchText(url));
-      this.scoring.set('task', task);
-
+    if (task) {
       // zoom map in on the task area
       let bbox = task.bbox.slice();
       scaleFromCenter(bbox, 0.3);
       let extent = transformExtent(bbox, EPSG_4326, EPSG_3857);
 
       this.mapService.map.getView().fit(extent);
+    }
+
+    if (filter.length !== 0) {
+      for (let row of filter) {
+        this.ws.subscribeToId(row.ID);
+      }
+
+      this.history.loadForIds(...filter.map(row => row.ID));
+    }
+  }
+
+  async loadDeviceFilter(url) {
+    if (url) {
+      await this.filter.load(url);
+    }
+
+    return this.filter.filter;
+  }
+
+  async loadTask(url) {
+    if (url) {
+      let { readTaskFromString } = await import('aeroscore/dist/src/read-task');
+      return readTaskFromString(await fetchText(url));
     }
   }
 }
