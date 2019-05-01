995796081595dc3564899499c89b0c9ba864d7b9
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-05-01 14:18:00 +0200

    Move `ScoreTable` from `index` to `application` route

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { alias } from '@ember/object/computed';
 
 export default class extends Controller {
+  @service filter;
   @service scoring;
+
+  @alias('filter.hasFilter') hasDeviceFilter;
 }
