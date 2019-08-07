2675e6c17069bd1a00a2c479c470319af48e0845
Dominic Spreitz <dominic@spreitz.de>
2019-08-07 21:48:18 +0200

    Improved device ID normlization in case of 6 digit IDs

--- app/utils/normalize-device-id.js
+++ app/utils/normalize-device-id.js
@@ -7,6 +7,22 @@ export function normalizeDeviceId(id) {
 
   if (length === 6) {
     if (RE_HEXDEC.test(id)) {
+      // Perform guestimates based on 6-digit device ID.
+      // In case device ID is in the range D00000 to DFFFFF it is most likely a FLARM ID
+      if (id.substring(0,1) == 'D') {
+        return `FLR${id}`;
+      // In case device ID is between 004000 to E94FFF it is most likely a ICAO-ID - this logic coule be elaborated more based on the country correlation: http://www.aerot$
+      // TODO express the above correctly
+      } else if (id.substring(0,1) == '3E') {
+        return `ICA${id}`;
+      // In case device ID is between 000000 to 003FFF 
+      // TODO: Express above correctly and replace '00'
+      } else if (id.substring(0,2) == '00') {
+        return `OGN${id.substring(2)}`;
+      } else {
+        // If nothing of the above matches assume FLARM as best guess
+        return `FLR${id}`;
+      }
       return `FLR${id}`;
     }
   } else if (length === 8) {
