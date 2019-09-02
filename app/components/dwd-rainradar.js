97d0719ba9c1f4dc76744d35d5b5e9eec30c151d
dspreitz <dominic@spreitz.de>
2019-09-02 23:42:54 +0200

    dwd-rainradar.ja: More Lint improvements.

--- app/components/dwd-rainradar.js
+++ app/components/dwd-rainradar.js
@@ -3,8 +3,8 @@ import Component from '@ember/component';
 import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
-import TileLayer from 'ol/layer/Tile'; 
-import TileWMS from 'ol/source/TileWMS'; 
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
 
 export default class extends Component {
   @service aircraft;
@@ -17,19 +17,19 @@ export default class extends Component {
   @alias('mapService.map') map;
 
   layer = new TileLayer({
-	visible: true,
-	source: new TileWMS({
-	url: 'https://maps.dwd.de:443/geoserver/dwd/wms',
-	params: {
-		'FORMAT': 'image/png',
-		'VERSION': '1.1.1',
-		tiled: true,
-		'LAYERS'": 'dwd:FX-Produkt',
-		'exceptions': 'application/vnd.ogc.se_inimage',
-		tilesOrigin: -15.074981 + "," + 34.7373759815055
-		},
-	opacity: 1.0,
-	}),
+    visible: true,
+    source: new TileWMS({
+      url: 'https://maps.dwd.de:443/geoserver/dwd/wms',
+      params: {
+        FORMAT: 'image/png',
+        VERSION: '1.1.1',
+        tiled: true,
+        LAYERS: 'dwd:FX-Produkt',
+        exceptions: 'application/vnd.ogc.se_inimage',
+        tilesOrigin: -15.074981 + ',' + 34.7373759815055,
+      },
+      opacity: 1.0,
+    }),
   });
 
   didInsertElement() {
