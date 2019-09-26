42ca7781175d5384cf67e9a07472a2322ad30bb4
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-09-26 22:31:30 +0200

    score-table: Use `@glimmer/component` base class

--- app/components/score-table.js
+++ app/components/score-table.js
@@ -1,6 +1,7 @@
-import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import Ember from 'ember';
 
 import * as Sentry from '@sentry/browser';
@@ -21,11 +22,9 @@ export default class extends Component {
   @service filter;
   @service history;
 
-  tagName = '';
-
   dayFactors = null;
   results = [];
-  rows = [];
+  @tracked rows = [];
 
   @computed('filter.filter')
   get initialDayFactors() {
@@ -56,7 +55,9 @@ export default class extends Component {
     let results = this.filter.filter.map(filterRow => {
       let fixes = this.history.forId(filterRow.id);
 
-      let solver = this.task.options.isAAT ? new AreaTaskSolver(this.task) : new RacingTaskSolver(this.task);
+      let solver = this.args.task.options.isAAT
+        ? new AreaTaskSolver(this.args.task)
+        : new RacingTaskSolver(this.args.task);
 
       let dayResult, landed, startTimestamp, altitude;
       try {
@@ -76,9 +77,9 @@ export default class extends Component {
         let H = filterRow.handicap / 100;
 
         dayResult =
-          landed || result.completed || this.task.options.isAAT
+          landed || result.completed || this.args.task.options.isAAT
             ? createInitialDayResult(result, this.initialDayFactors, H)
-            : createIntermediateDayResult(result, this.initialDayFactors, H, this.task, Date.now() / 1000);
+            : createIntermediateDayResult(result, this.initialDayFactors, H, this.args.task, Date.now() / 1000);
       } catch (error) {
         Sentry.captureException(error);
 
@@ -95,28 +96,25 @@ export default class extends Component {
       return { ...dayResult, landed, filterRow, startTimestamp, altitude };
     });
 
-    this.set('dayFactors', calculateDayFactors(results, this.initialDayFactors));
+    this.dayFactors = calculateDayFactors(results, this.initialDayFactors);
 
-    this.set('results', results.map(result => calculateDayResult(result, this.dayFactors)).sort(compareDayResults));
+    this.results = results.map(result => calculateDayResult(result, this.dayFactors)).sort(compareDayResults);
 
-    this.set(
-      'rows',
-      this.results.map((result, i) => {
-        let { filterRow } = result;
+    this.rows = this.results.map((result, i) => {
+      let { filterRow } = result;
 
-        return {
-          num: `${result.landed || result._completed ? ' ' : '!'} ${i + 1}`,
-          cn: filterRow.callsign,
-          name: filterRow.name,
-          type: filterRow.type,
-          startTime: result.startTimestamp ? formatTime(result.startTimestamp) : '',
-          time: result._T ? formatDuration(result._T) : '',
-          distance: result._D ? `${result._D.toFixed(1)} km` : '',
-          speed: result._V ? `${result._V.toFixed(2)} km/h` : '',
-          score: result.S,
-          altitude: result.altitude !== null ? `${Math.round(result.altitude)} m` : '',
-        };
-      }),
-    );
+      return {
+        num: `${result.landed || result._completed ? ' ' : '!'} ${i + 1}`,
+        cn: filterRow.callsign,
+        name: filterRow.name,
+        type: filterRow.type,
+        startTime: result.startTimestamp ? formatTime(result.startTimestamp) : '',
+        time: result._T ? formatDuration(result._T) : '',
+        distance: result._D ? `${result._D.toFixed(1)} km` : '',
+        speed: result._V ? `${result._V.toFixed(2)} km/h` : '',
+        score: result.S,
+        altitude: result.altitude !== null ? `${Math.round(result.altitude)} m` : '',
+      };
+    });
   }
 }
